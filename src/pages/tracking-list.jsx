d6ef3ed852aaafad68d55d56e03de40c7416919b
abdurahmonrahmatjonov <abdurahmonrahmatjonov89@gmail.com>
2024-06-24 11:02:57 +0500

    fix: added editing

--- src/pages/tracking-list.jsx
+++ src/pages/tracking-list.jsx
@@ -1,163 +1,49 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
-import { Input, Button } from "antd";
+import {  Button } from "antd";
 import Menubar from "../components/Menu";
 import { useSelector } from "react-redux";
 import { http } from "../services/http";
-import { SearchOutlined } from "@ant-design/icons";
-import Highlighter from "react-highlight-words";
 import { useTranslation } from "react-i18next";
-import { column } from "../utils/columns";
 import { aggregateDocuments } from "../utils/document";
 import moment from "moment";
 import "jspreadsheet-ce/dist/jspreadsheet.css";
 import jspreadsheet from "jspreadsheet-ce";
-import Navbar from '../components/navbar';
+import Navbar from "../components/navbar";
 
 const TrackingList = () => {
   const employeId = useSelector((state) => state.main.employeeId);
   const { t } = useTranslation();
   const spreadsheetRef = useRef(null);
-  const [fdata, setFData] = useState([]);
-  const [loading, setLoading] = useState(true);
   const jspreadsheetInstanceRef = useRef(null);
-
+  const [fdata, setFData] = useState([]);
+  const [_, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [hasMoreData, setHasMoreData] = useState(false);
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
   const [isEditable, setIsEditable] = useState(false);
 
-  let searchInput = null;
-
-  const handleSearch = (e, confirm, dataIndex) => {
-    const value = e.target.value;
-  };
-
-  const getColumnSearchProps = (dataIndex) => {
-    return {
-      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm }) => (
-        <div style={{ padding: 8 }}>
-          <Input
-            ref={(node) => {
-              searchInput = node;
-            }}
-            placeholder={`${t("search")} ${dataIndex}`}
-            value={selectedKeys[0]}
-            onChange={(e) => {
-              setSelectedKeys(e.target.value ? [e.target.value] : []);
-              handleSearch(e, confirm, dataIndex);
-            }}
-            style={{ width: 188, marginBottom: 8, display: "block" }}
-          />
-        </div>
-      ),
-      filterIcon: (filtered) => (
-        <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
-      ),
-      onFilter: (value, record) =>
-        record[dataIndex]
-          ? record[dataIndex]
-              .toString()
-              .toLowerCase()
-              .includes(value.toLowerCase())
-          : "",
-      onFilterDropdownVisibleChange: (visible) => {
-        if (visible) {
-          setTimeout(() => searchInput.select(), 100);
-        }
-      },
-      render: (text) =>
-        searchedColumn === dataIndex ? (
-          <Highlighter
-            highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-            searchWords={[searchText]}
-            autoEscape
-            textToHighlight={text ? text.toString() : ""}
-          />
-        ) : (
-          text
-        ),
-    };
-  };
-
-  const columns = column(t, getColumnSearchProps).map((col) => {
-    if (col.dataIndex === "summa") {
-      return {
-        ...col,
-        render: (text, record) => {
-          const formattedText = new Intl.NumberFormat("fr-FR").format(
-            record.summa,
-          );
-          return `${formattedText} ${record.currency && Array.isArray(record.currency) ? record.currency[0] : ''}`;
-        },
-      };
-    }
-
-    if (col.dataIndex === "docNum") {
-      return {
-        ...col,
-        title: t("numSale"),
-      };
-    }
-
-    return col;
-  });
-
-  const fetchData = useCallback(
-    async (page) => {
-      try {
-        setLoading(true);
-        const { data } = await http.get(
-          `api/meningsotuvlarim?ownerCode=${employeId}&pageToken=${page}`,
-        );
-
-        const formattedData = aggregateDocuments(data);
-        console.log(formattedData);
-        const maxsulotLengths = formattedData.map(
-          (entry) => entry.maxsulot.length,
-        );
-        const totalMaxsulotLength = maxsulotLengths.reduce(
-          (sum, length) => sum + length,
-          0,
-        );
-
-        const hasMore = totalMaxsulotLength === 10 ? true : false;
-        setFData(formattedData);
-        setHasMoreData(hasMore);
-      } catch (error) {
-        console.error(error);
-      }
-      setLoading(false);
-    },
-    [employeId],
-  );
-
-  const handleSave = () => {
-    if (!jspreadsheetInstanceRef.current) {
-      return; // If no spreadsheet instance exists, exit early
-    }
-
-    const updatedData = jspreadsheetInstanceRef.current.getData(); // Get updated data from the spreadsheet instance
-    console.log("Updated Data:", updatedData);
-
-    // Example: Send updated data to a server using HTTP POST request
-    saveUpdatedData(updatedData);
-  };
-
-  const saveUpdatedData = async (data) => {
+  const fetchData = useCallback(async (page) => {
     try {
       setLoading(true);
-      // Example: Replace with your actual HTTP POST request to save data
-      const response = await http.post('api/saveData', { data });
-      console.log("Save response:", response.data); // Log response or handle success message
-      // Optionally, update local state or perform additional actions upon successful save
+      const { data } = await http.get(
+        `api/meningsotuvlarim?ownerCode=${employeId}&pageToken=${page}`
+      );
+      const formattedData = aggregateDocuments(data);
+      const maxsulotLengths = formattedData.map(
+        (entry) => entry.maxsulot && entry.maxsulot.length
+      );
+      const totalMaxsulotLength = maxsulotLengths.reduce(
+        (sum, length) => sum + (length || 0),
+        0
+      );
+      const hasMore = totalMaxsulotLength === 10;
+      setFData(formattedData);
+      setHasMoreData(hasMore);
     } catch (error) {
-      console.error("Error saving data:", error);
-      // Handle error state or display error message to user
+      console.error("Error fetching data:", error);
     } finally {
       setLoading(false);
     }
-  };
+  }, [employeId]);
 
   const handleNextPage = () => {
     if (hasMoreData) {
@@ -171,34 +57,65 @@ const TrackingList = () => {
     }
   };
 
+  const toggleEdit = () => {
+    setIsEditable((prevEditable) => !prevEditable);
+    // Save data when toggling edit mode
+    handleSave();
+  };
+
+  const handleSave = () => {
+    if (!jspreadsheetInstanceRef.current) {
+      return;
+    }
+    const updatedData = jspreadsheetInstanceRef.current.getData();
+    console.log("Updated Data:", updatedData);
+    saveUpdatedData(updatedData);
+  };
+
+  const saveUpdatedData = async (data) => {
+    try {
+      setLoading(true);
+      const response = await http.post("api/saveData", { data });
+      console.log("Save response:", response.data);
+    } catch (error) {
+      console.error("Error saving data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [fetchData, currentPage]);
+
   useEffect(() => {
     const tableData = fdata.map((item) => ({
       ...item,
-      maxsulot: item.maxsulot.join(", "),
-      sana: moment(item.sana, "DD.MM.YYYY").format("DD.MM.YYYY, HH:mm:ss"), // Specify date format
+      maxsulot: item.maxsulot ? item.maxsulot.join(", ") : "",
+      sana: item.sana ? moment(item.sana, "DD.MM.YYYY").format("DD.MM.YYYY, HH:mm:ss") : "",
     }));
 
-    if (spreadsheetRef.current) {
+    if (spreadsheetRef.current && tableData.length > 0) {
       if (jspreadsheetInstanceRef.current) {
         jspreadsheetInstanceRef.current.destroy();
       }
 
-      jspreadsheetInstanceRef.current = jspreadsheet(spreadsheetRef.current, {
-        data: tableData.map((row) => columns.map((col) => row[col.dataIndex])),
-        columns: columns.map((col) => ({
+      const jSpreadsheetOptions = {
+        data: tableData.map((row) =>
+          Object.values(row).map((value) => value || "")
+        ),
+        columns: Object.keys(tableData[0] || {}).map((key) => ({
           type: "text",
-          title: col.title,
-          width: col.width || 150,
+          title: key,
+          width: 150,
         })),
-        minDimensions: [columns.length, tableData.length],
+        minDimensions: [Object.keys(tableData[0] || {}).length, tableData.length],
         editable: isEditable,
-      });
+      };
+
+      jspreadsheetInstanceRef.current = jspreadsheet(spreadsheetRef.current, jSpreadsheetOptions);
     }
-  }, [fdata, columns, isEditable]);
-  // Fetch data on initial load
-  useEffect(() => {
-    fetchData(currentPage);
-  }, [fetchData, currentPage]);
+  }, [fdata, isEditable]);
 
   return (
     <div className="flex h-full w-full overflow-y-auto">
@@ -232,7 +149,7 @@ const TrackingList = () => {
             </div>
           </div>
           <Button
-            onClick={handleSave}
+            onClick={toggleEdit}
             className="mb-4 bg-blue-500 text-white hover:bg-blue-700"
           >
             {isEditable ? t("Save") : t("Edit")}
